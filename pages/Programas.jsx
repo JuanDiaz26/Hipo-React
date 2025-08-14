c14bfec98db80defd55569fabd95e00e0f750ccb
Juan Gabriel Diaz <juanbacanp3s@gmail.com>
2025-08-14 12:16:40 -0300

    terminando la parte programas con links, todo solucionado ya, error en img con tema @import

--- pages/Programas.jsx
+++ pages/Programas.jsx
@@ -4,30 +4,19 @@ import Navbar from "../components/Navbar";
 import "./TablasPages.css";
 
 const programas = [
-  {
-    nombre: "Programa nº 14 - 13 de Julio",
-    link: "https://drive.google.com/archivo-13julio",
-  },
-  {
-    nombre: "Programa nº 15 - 27 de Julio",
-    link: "https://drive.google.com/archivo-27julio",
-  },
-  {
-    nombre: "Programa nº 16 - 10 de Agosto",
-    link: "https://drive.google.com/archivo-10agosto",
-  },
-  {
-    nombre: "Programa nº 17 - 24 de Agosto",
-    link: "https://drive.google.com/archivo-24agosto",
-  },
-  {
-    nombre: "Programa nº 18 - 7 de Septiembre",
-    link: "https://drive.google.com/archivo-7septiembre",
-  },
-  {
-    nombre: "Programa nº 19 - 24 de Septiembre",
-    link: "https://drive.google.com/archivo-24septiembre",
-  },
+  { nombre: "Reunión nº 13 - 10 de Agosto", link: "https://drive.google.com/file/d/1UwOKeZVKwGaAkgt12TK28m0RBH2_MzMC/view?usp=sharing" },
+  { nombre: "Reunión nº 12 - 27 de Julio", link: "https://drive.google.com/file/d/1tB5CgH3XZFxDm-4yDFulhpQWYaX0E75Z/view?usp=sharing" },
+  { nombre: "Reunión nº 11 - 13 de Julio", link: "https://drive.google.com/file/d/1Jenn38Q2T8i9FQXLokq5tOaiumtYGS9l/view?usp=sharing" },
+  { nombre: "Reunión nº 9 - 22 de Junio",  link: "https://drive.google.com/file/d/15Lf-9G_B8z83uSYMnmThfxPwwjKIDlPc/view?usp=sharing" },
+  { nombre: "Reunión nº 10 - 8 de Junio", link: "https://drive.google.com/file/d/1ZXKEG-CIXQBuwmpSwiwwFJcfgp21LLEG/view?usp=sharing" },
+  { nombre: "Reunión nº 8 - 25 de Mayo",  link: "https://drive.google.com/file/d/14fjaoVEidIiWddlH2bJEX9Y0ypla5CWj/view?usp=sharing" },
+  { nombre: "Reunión nº 7 - 11 de Mayo",  link: "https://drive.google.com/file/d/1CH5JhMxyzvX_9eYNMIDtCmrc83uOySJa/view?usp=sharing" },
+  { nombre: "Reunión nº 6 - 26 de Abril",  link: "https://drive.google.com/file/d/1p6vHcmqwjapDWohcEfcHefplnO47cI0z/view?usp=sharing" },
+  { nombre: "Reunión nº 5 - 13 de Abril",  link: "https://drive.google.com/file/d/1ZKj4olqLVupPFuZb7Vh1CFN3oKTbsAqR/view?usp=sharing" },
+  { nombre: "Reunión nº 4 - 30 de Marzo",  link: "https://drive.google.com/file/d/1cEy87_EJFiRbXlI9Ee4ni6SVSnAjZ8Tz/view?usp=sharing" },
+  { nombre: "Reunión nº 3 - 16 de Marzo",  link: "https://drive.google.com/file/d/1yr2U6VwysPQbn4l4CrxTKA0QaWmPjmzA/view?usp=sharing" },
+  { nombre: "Reunión nº 2 - 23 de Febrero", link: "https://drive.google.com/file/d/1X-8tLkMN9bxhz_US0exew6eGeFZee4Xn/view?usp=sharing" },
+  { nombre: "Reunión nº 1 - 16 de Febrero", link: "https://drive.google.com/file/d/1Wnkb-7-rVsueYN7N7RKJnAeine3kAW7k/view?usp=sharing" },
 ];
 
 const Programas = () => {
